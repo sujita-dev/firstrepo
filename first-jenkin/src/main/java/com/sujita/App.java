72e41b2ae906c64d9131cd4320eaa4ab5e409c85
sujita <HP@DESKTOP-36MG083>
2021-12-16 02:38:15 +0530

    First maven projet

---
+++ first-jenkin/src/main/java/com/sujita/App.java
@@ -0,0 +1,9 @@
+package com.sujita;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Sujita Patil");
+	}
+
+}
---
+++ first-jenkin/src/test/java/com/sujita/AppTest.java
@@ -0,0 +1,18 @@
+package com.sujita;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+	
+	
+	
+	
+
+}
